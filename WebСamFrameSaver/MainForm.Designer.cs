55e0af3942884ced2aad003a0bf19eacbce89217
Vladimir-Danilin <121000472+Vladimir-Danilin@users.noreply.github.com>
2025-01-29 19:21:16 +0600

    Добавьте файлы проекта.

---
+++ WebСamFrameSaver/MainForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WebСamFrameSaver
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboboxCameraDevices = new System.Windows.Forms.ComboBox();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.labelResolution = new System.Windows.Forms.Label();
+            this.labelFps = new System.Windows.Forms.Label();
+            this.buttonChangeDirectory = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboboxCameraDevices
+            // 
+            this.comboboxCameraDevices.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboboxCameraDevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboboxCameraDevices.FormattingEnabled = true;
+            this.comboboxCameraDevices.Location = new System.Drawing.Point(652, 27);
+            this.comboboxCameraDevices.Name = "comboboxCameraDevices";
+            this.comboboxCameraDevices.Size = new System.Drawing.Size(120, 21);
+            this.comboboxCameraDevices.TabIndex = 1;
+            this.comboboxCameraDevices.SelectedIndexChanged += new System.EventHandler(this.comboboxCameraDevices_SelectedIndexChanged);
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.BackColor = System.Drawing.SystemColors.Control;
+            this.videoSourcePlayer.Location = new System.Drawing.Point(12, 27);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(634, 422);
+            this.videoSourcePlayer.TabIndex = 2;
+            this.videoSourcePlayer.Text = "videoSourcePlayer";
+            this.videoSourcePlayer.VideoSource = null;
+            this.videoSourcePlayer.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer_NewFrame);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPlay.Location = new System.Drawing.Point(652, 368);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(120, 23);
+            this.buttonPlay.TabIndex = 3;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSave.Location = new System.Drawing.Point(652, 397);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(120, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // labelResolution
+            // 
+            this.labelResolution.AutoSize = true;
+            this.labelResolution.BackColor = System.Drawing.Color.Transparent;
+            this.labelResolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelResolution.Location = new System.Drawing.Point(12, 9);
+            this.labelResolution.Name = "labelResolution";
+            this.labelResolution.Size = new System.Drawing.Size(93, 15);
+            this.labelResolution.TabIndex = 5;
+            this.labelResolution.Text = "labelResolution";
+            // 
+            // labelFps
+            // 
+            this.labelFps.AutoSize = true;
+            this.labelFps.BackColor = System.Drawing.Color.Transparent;
+            this.labelFps.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelFps.Location = new System.Drawing.Point(171, 9);
+            this.labelFps.Name = "labelFps";
+            this.labelFps.Size = new System.Drawing.Size(57, 15);
+            this.labelFps.TabIndex = 6;
+            this.labelFps.Text = "labelFPS";
+            // 
+            // buttonChangeDirectory
+            // 
+            this.buttonChangeDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonChangeDirectory.Location = new System.Drawing.Point(652, 426);
+            this.buttonChangeDirectory.Name = "buttonChangeDirectory";
+            this.buttonChangeDirectory.Size = new System.Drawing.Size(120, 23);
+            this.buttonChangeDirectory.TabIndex = 7;
+            this.buttonChangeDirectory.Text = "Change directory";
+            this.buttonChangeDirectory.UseVisualStyleBackColor = true;
+            this.buttonChangeDirectory.Click += new System.EventHandler(this.buttonChangeDirectory_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.buttonChangeDirectory);
+            this.Controls.Add(this.labelFps);
+            this.Controls.Add(this.labelResolution);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.comboboxCameraDevices);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(410, 200);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrameSaver";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboboxCameraDevices;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Label labelResolution;
+        private System.Windows.Forms.Label labelFps;
+        private System.Windows.Forms.Button buttonChangeDirectory;
+    }
+}
+
---
+++ WebСamFrameSaver/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using AForge.Controls;
+using AForge.Imaging.Filters;
+using AForge.Video.DirectShow;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+
+namespace WebСamFrameSaver
+{
+    public partial class MainForm : Form
+    {
+        private VideoCaptureDevice videoCaptureSource = null;
+        private FilterInfoCollection videoDevices;
+
+        private int saveFrameCounter;
+        private double currentFps;
+        private string currentResolution;
+        private DateTime lastFrameTime;
+        private readonly Timer uiUpdateTimer;
+        private string savePath;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            comboboxCameraDevices.Items.Clear();
+            foreach (FilterInfo i in videoDevices) comboboxCameraDevices.Items.Add(i.Name);
+            comboboxCameraDevices.SelectedIndex = 0;
+
+            uiUpdateTimer = new Timer { Interval = 100 };
+            uiUpdateTimer.Tick += (s, e) => { labelFps.Text = $"FPS: {Math.Round(currentFps)}"; };
+            uiUpdateTimer.Start();
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            videoCaptureSource = null;
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+
+            videoCaptureSource = new VideoCaptureDevice(videoDevices[comboboxCameraDevices.SelectedIndex].MonikerString);
+            videoSourcePlayer.VideoSource = videoCaptureSource;
+            videoSourcePlayer.Start();
+
+            var resolution = videoCaptureSource.VideoCapabilities
+            .OrderByDescending(vc => vc.FrameSize.Width * vc.FrameSize.Height)
+            .First();
+            currentResolution = $"{resolution.FrameSize.Width}x{resolution.FrameSize.Height}";
+            labelResolution.Text = $"Resolution: {currentResolution}";
+
+            FormBorderStyle = FormBorderStyle.Sizable;
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (savePath != null)
+                for (int i = saveFrameCounter; ; i++)
+                {
+                    string fullPath = Path.Combine(savePath, $"SaveFrame{i}.bmp");
+
+                    if (!File.Exists(fullPath))
+                    {
+                        if (videoSourcePlayer.IsRunning)
+                            videoSourcePlayer.GetCurrentVideoFrame().Save(fullPath);
+                        else
+                            MessageBox.Show($"Failed to save {fullPath}");
+                        saveFrameCounter = i;
+                        return;
+                    }
+                }
+            else
+                MessageBox.Show("Select directory");
+        }
+
+        private void buttonChangeDirectory_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.Cancel | File.Exists(folderBrowserDialog.SelectedPath))
+                MessageBox.Show("Unacceptable path");
+            else
+            {
+                saveFrameCounter = 1;
+                savePath = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void videoSourcePlayer_NewFrame(object sender, ref Bitmap image)
+        {
+            using (Bitmap frame = image.Clone() as Bitmap)
+            {
+                if (frame == null) return;
+
+                Grayscale grayscaleFilter = new Grayscale(0.2126, 0.7152, 0.0722);
+                var grayFrame = grayscaleFilter.Apply(frame);
+                image = grayFrame;
+
+                DateTime currentTime = DateTime.Now;
+                if (lastFrameTime != DateTime.MinValue) 
+                    currentFps = 1000f / (currentTime - lastFrameTime).TotalMilliseconds;
+
+                lastFrameTime = currentTime;
+            }
+        }
+
+        private void comboboxCameraDevices_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (FormBorderStyle == FormBorderStyle.Sizable)
+            {
+                int resizeableWidth = Width - (videoSourcePlayer.Location.X + buttonPlay.Width + buttonPlay.Margin.Horizontal + 40);
+                int resizeableHeight = Height - videoSourcePlayer.Location.Y - 45;
+
+                var resolution = videoCaptureSource.VideoCapabilities
+                .OrderByDescending(vc => vc.FrameSize.Width * vc.FrameSize.Height)
+                .First();
+                float resolutionScale = (float)resolution.FrameSize.Width / resolution.FrameSize.Height;
+
+                float formContainerResolution = (float)resizeableWidth / resizeableHeight;
+
+                if (formContainerResolution > resolutionScale)
+                {
+                    videoSourcePlayer.Height = resizeableHeight;
+                    videoSourcePlayer.Width = (int)(resizeableHeight * resolutionScale);
+                }
+                else
+                {
+                    videoSourcePlayer.Height = (int)(resizeableWidth / resolutionScale);
+                    videoSourcePlayer.Width = resizeableWidth;
+                }
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+        }
+    }
+}
